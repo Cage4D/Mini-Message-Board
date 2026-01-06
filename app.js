22d4e55e42b9eb7b1e3c4ba3c3f0c202dcc84c7c
Johnny Cage <thespammingbott@gmail.com>
2026-01-06 17:28:49 +0100

    feat: parse form data into req.body

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const newRouteRouter = require("./routes/newRoute")
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+app.use(express.urlencoded({ extended: true }))
 app.use("/", indexRouter)
 app.use("/new", newRouteRouter)
