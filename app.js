7bb055f622875dc314adbf90225a89867cbb1e38
Johnny Cage <thespammingbott@gmail.com>
2026-01-06 16:53:24 +0100

    feat: configure the views path and view engine

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
+const path = require("node:path")
 const indexRouter = require("./routes/index")
 const newRouteRouter = require("./routes/newRoute")
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
 app.use("/", indexRouter)
 app.use("/new", newRouteRouter)
