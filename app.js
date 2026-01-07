18344b2965b671c17b24a14383cc7e6e06fbef28
Johnny Cage <thespammingbott@gmail.com>
2026-01-07 05:31:42 +0100

    feat: messageRoute handler and port listener

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require("express")
-const app = express()
-const path = require("node:path")
-const indexRouter = require("./routes/index")
-const newRouteRouter = require("./routes/newRoute")
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const PORT = 3000;
+const indexRouter = require("./routes/index");
+const newRouteRouter = require("./routes/newRoute");
+const messageRouter = require("./routes/messageViewRoute");
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }))
-app.use("/", indexRouter)
-app.use("/new", newRouteRouter)
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+app.use("/new", newRouteRouter);
+app.use("/message", messageRouter);
+
+app.listen(PORT, (error) => {
+  if (error) throw error;
+  console.log(`listening on PORT ${PORT}`);
+});
