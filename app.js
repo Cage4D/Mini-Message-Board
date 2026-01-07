aea683116a4fe8ed9f6a8c6e00102baddcce12ec
Johnny Cage <thespammingbott@gmail.com>
2026-01-08 00:05:15 +0100

    feat: configure express static files path

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const PORT = 3000;
 const indexRouter = require("./routes/index");
 const newRouteRouter = require("./routes/newRoute");
 const messageRouter = require("./routes/messageViewRoute");
+const assetsPath = path.join(__dirname, "public")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath))
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouteRouter);
