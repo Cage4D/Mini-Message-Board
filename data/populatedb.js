0ae0f53ce0bc68200bb2946c104c72fb5e111f57
Johnny Cage <thespammingbott@gmail.com>
2026-01-12 20:53:11 +0100

    feat: database populating script

---
+++ data/populatedb.js
@@ -0,0 +1,29 @@
+const { Client } = require("pg")
+require("dotenv").config()
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        text TEXT NOT NULL,
+        user VARCHAR ( 255 ) NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+
+    INSERT INTO messages (text, user , date)
+    VALUES 
+    ('Hi there!', 'AMANDO'),
+    ('Hello World!', 'Charles');
+`;
+
+async function main() {
+    console.log("seeding....")
+    const client = new Client({
+        connectionString: process.env.CONNECTION_STRING,
+    })
+    await client.connect()
+    await client.query(SQL)
+    await client.end()
+    console.log("done")
+}
+
+main()
