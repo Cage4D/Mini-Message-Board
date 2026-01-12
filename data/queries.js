1096b8640795c73de073b594a7b36e2fd7e7915d
Johnny Cage <thespammingbott@gmail.com>
2026-01-12 21:02:46 +0100

    feat: implement asynchronous functions to retrieve messages from the database

---
+++ data/queries.js
@@ -0,0 +1,10 @@
+const pool = require("./pool")
+
+async function retrieveAllMessages() {
+    const { rows } = await pool.query("SELECT * FROM messages")
+    return rows;
+}
+
+module.exports = {
+    retrieveAllMessasges
+}
