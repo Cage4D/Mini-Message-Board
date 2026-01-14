dccd023952200616bef344241a6b93d5a9238eac
Johnny Cage <thespammingbott@gmail.com>
2026-01-14 19:34:23 +0100

    refactor: messages are now fetched from db instead of a static array

--- routes/newRoute.js
+++ routes/newRoute.js
@@ -1,16 +1,15 @@
 const { Router } = require("express")
 const newRouter = Router()
-const messages = require("../data/messages")
+const messages = require("../data/queries")
 
 
 newRouter.get("/", (req, res) => {
     res.render("form", {})
 })
 
-newRouter.post("/", (req, res) => {
-    const messageText = req.body.messageText
-    const authorName = req.body.authorName
-    messages.push({ text: messageText, user: authorName, added: new Date() })
+newRouter.post("/", async (req, res) => {
+    const { messageText, authorName } = req.body
+   await messages.writeMessage({ text: messageText, author: authorName })
     res.redirect("/")
 })
 
