94d381ed83f7c1992e11589db751fa7dcb1f8557
Johnny Cage <thespammingbott@gmail.com>
2026-01-14 19:38:50 +0100

    feat: make get callback function asynchronous

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express")
 const indexRouter = Router()
-const messages = require("../data/messages")
+const messages = require("../data/queries")
 
-indexRouter.get("/", (req, res) => {
+indexRouter.get("/", async (req, res) => {
     res.render("index", {
         title: "Mini Messageboard",
-        messages: messages,
+        messages: await messages.retrieveAllMessages(),
     })
 })
 
